9e89c10c6aa7752fbac113a5d50dca50f7e3667a
Richard T Swierk <richard.swierk@mymail.champlain.edu>
2020-03-29 15:03:33 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!bin/bash
 sudo apt-get update
+#install ssmtp and mailutils
 sudo apt-get install ssmtp mailutils --yes
 sudo apt-get update --fix-missing
 sudo apt-get install ssmtp mailutils --yes
+#install network manager
+sudo apt-get install network-manager --yes
+#install create_ap
+git clone https://github.com/oblique/create_ap.git
+cd create_ap
+sudo make install
+#Make pocketPi executable
 chmod +x pocketPi.sh
