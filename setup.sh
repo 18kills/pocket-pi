fd7bea0023b51802258d8b9e8c8de38b920baf9f
Richard T Swierk <richard.swierk@mymail.champlain.edu>
2020-03-29 17:00:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-install(){
+#Install needed packages
+installPackages(){
   sudo apt-get update
   #install ssmtp and mailutils
   sudo apt-get install ssmtp mailutils --yes
@@ -13,31 +14,41 @@ install(){
   cd create_ap
   sudo make install
 }
-
-#Make pocketPi executable
-chmod +x PocketPi
-#Copy file to /etc
-sudo cp /home/pi/pocket-pi/PocketPi /etc/PocketPi
-#Edit /etc/rc.local to run the PocketPi program on boot
-sudo echo "sudo ./etc/PocketPi & exit 0" 
-
-#Setup ssmtp to send mail through a gmail account
-email="user@anything.com"
-emailPass="password"
-#User inputs email account
-echo "If you are using a gmail account make sure that less secure app access is on"
-echo "Input Example: user@gmail.com"
-read -p "Enter your full email address: " email
-read -p "Enter your password to your email: " emailPass
-#Get the mailhub address
-emailProvider=${email#*@}
-emailProvider=${emailProvider%.*}
-mailhub=$( cat MailServers | grep "$emailProvider" )
-#Make the ssmtp config file
-sudo echo "root=$email" > /etc/ssmtp/ssmtp.conf
-sudo echo "mailhub=$mailhub" >> /etc/ssmtp/ssmtp.conf
-sudo echo "hostname=$(hostname)" >> /etc/ssmtp/ssmtp.conf
-sudo echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
-sudo echo "AuthUser=$email" >> /etc/ssmtp/ssmtp.conf
-sudo echo "AuthPass=$emailPass" >> /etc/ssmtp/ssmtp.conf
-sudo echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
+#Configure the PocketPi program
+pocketPiConf(){
+	#Make pocketPi executable
+	chmod +x PocketPi
+	#Copy file to /etc
+	sudo cp /home/pi/pocket-pi/PocketPi /etc/PocketPi
+	#Edit /etc/rc.local to run the PocketPi program on boot
+	sudo echo "sudo ./etc/PocketPi $email & exit 0" > /etc/rc.local
+}
+#Setup mailing
+MailSetup(){
+	#Setup ssmtp to send mail through a gmail account
+	email="user@anything.com"
+	emailPass="password"
+	#User inputs email account
+	echo "If you are using a gmail account make sure that less secure app access is on"
+	echo "Input Example: user@gmail.com"
+	read -p "Enter your full email address: " email
+	read -p "Enter your password to your email: " emailPass
+	#Get the mailhub address
+	emailProvider=${email#*@}
+	emailProvider=${emailProvider%.*}
+	mailhub=$( cat MailServers | grep "$emailProvider" )
+	#Make the ssmtp config file
+	sudo echo "root=$email" > /etc/ssmtp/ssmtp.conf
+	sudo echo "mailhub=$mailhub" >> /etc/ssmtp/ssmtp.conf
+	sudo echo "hostname=$(hostname)" >> /etc/ssmtp/ssmtp.conf
+	sudo echo "FromLineOverride=YES" >> /etc/ssmtp/ssmtp.conf
+	sudo echo "AuthUser=$email" >> /etc/ssmtp/ssmtp.conf
+	sudo echo "AuthPass=$emailPass" >> /etc/ssmtp/ssmtp.conf
+	sudo echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf
+}
+#Call the installPackages function
+installPackages
+#Call MailSetup function
+MailSetup
+#Call the pocketPiConf function
+pocketPiConf
